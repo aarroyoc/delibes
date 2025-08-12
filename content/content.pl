a344a2d23bbb061625798124756d9a8a1ebece8c
Adrián Arroyo Calle <aarroyoc@adrianistan.eu>
2025-08-12 22:17:25 +0200

    Paralelismo para lelos parte 2

--- content/content.pl
+++ content/content.pl
@@ -339,3 +339,4 @@ post("que-paso-en-2024", "¿Qué pasó en 2024?", aarroyoc, "2025-01-03 18:20:00
 post("entrevista-tecnolocuras", "Entrevista tecnolocuras.com", aarroyoc, "2025-03-06 23:07:00.00000", html("entrevista-tecnolocuras.html"), public, ["blog", "entrevista", "personal"]).
 post("365-dias-de-chino-con-duolingo", "365 días de Chino con Duolingo", aarroyoc, "2025-04-25 20:35:00.00000", html("365-dias-de-chino-con-duolingo.html"), public, ["idiomas", "chino", "personal"]).
 post("paralelismo-para-lelos", "Paralelismo para lelos", aarroyoc, "2025-07-07 22:27:00.00000", html("paralelismo-para-lelos.html"), public, ["programacion", "rust", "paralelismo"]).
+post("paralelismo-para-lelos-2", "Paralelismo para lelos parte 2", aarroyoc, "2025-08-12 22:17:00.00000", html("paralelismo-para-lelos-2.html"), public, ["programacion", "rust", "paralelismo"]).
